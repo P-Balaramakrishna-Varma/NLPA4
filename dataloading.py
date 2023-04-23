93e7f1556cc518407c2160798ca9473403dc7dd3
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-04-23 08:05:59 +0530

    sst dataset done

--- dataloading.py
+++ dataloading.py
@@ -3,7 +3,7 @@ from nltk.tokenize import word_tokenize
 import torch
 import datasets
 import torchtext
-
+import string
 
 
 def preprocess_sentence(sentence, ps):
@@ -24,10 +24,28 @@ def create_vocab_stt(dataset):
     vocab.set_default_index(vocab["<unk>"])
     return vocab
 
+class SSTDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset, vocab):
+        super().__init__()
+        self.dataset = dataset
+        self.vocab = vocab
+        self.ps = PorterStemmer()
 
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        sample = self.dataset[idx]
+        x = preprocess_sentence(sample['sentence'], self.ps)
+        x = [self.vocab[token] for token in x]
+        y = round(sample['label']) + 1     # 0 -> 1, 1 -> 2 (for cross entropy) class 0 for padding
+        return torch.tensor(x, dtype=torch.long), torch.tensor(y, dtype=torch.long)
 
 
 
 if __name__ == '__main__':
     dataset = datasets.load_dataset('sst')
     vocab = create_vocab_stt(dataset)
+    train_dataset = SSTDataset(dataset["train"], vocab)
+    print(len(dataset["train"]), len(train_dataset))
+    print(train_dataset[0])
