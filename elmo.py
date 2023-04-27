0e7efb953b9ca8b19192f170872e7568f4846ebb
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-04-27 15:28:45 +0530

    sst trainning done

--- elmo.py
+++ elmo.py
@@ -63,7 +63,7 @@ class ELMoSentiment(torch.nn.Module):
 
 def train_stt(model, train_loader, optimizer, loss_func, device):
     model.train()
-    for X, y in tqdm(train_loader):
+    for X, y in train_loader:
         # data gathering
         X, y = X.to(device), y.to(device)
         
@@ -94,17 +94,34 @@ def eval_stt(model, dataloader, loss_func, device):
     return loss / count   
 
 
+
 if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
+    # Data creation part
     dataset = datasets.load_dataset('sst')
     vocab = create_vocab_stt(dataset["train"])
+    
     train_dataset = SSTDataset(dataset["train"], vocab)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_stt)
+    test_dataset = SSTDataset(dataset["test"], vocab)
+    valid_dataset = SSTDataset(dataset["validation"], vocab)
 
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_stt)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_stt)
+    valid_loader = torch.utils.data.DataLoader(valid_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_stt)
+
+    # Model, optimizer, loss funnction, hyperparameters
     model = ELMoSentiment(len(vocab), 300, 400, 800).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.0001)
     loss_func = torch.nn.CrossEntropyLoss()
+    epochs = 10
 
-    train_stt(model, train_loader, optimizer, loss_func, device)
-    print(eval_stt(model, train_loader, loss_func, device))
+    # Training
+    train_losses = []
+    valid_losses = []
+    for epoch in tqdm(range(epochs)):
+        train_stt(model, train_loader, optimizer, loss_func, device)
+        train_losses.append(eval_stt(model, train_loader, loss_func, device))
+        valid_losses.append(eval_stt(model, valid_loader, loss_func, device))
+    
+    print(train_losses, valid_losses)
