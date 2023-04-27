c3619a499d6da0247e51ec460f1cd09db8eb341a
P Balaramakrishna Varma <balaramakrishna.p@students.iiit.ac.in>
2023-04-27 15:03:16 +0530

    Model definiation

--- elmo.py
+++ elmo.py
@@ -7,17 +7,59 @@ import torch
 class ELMo(torch.nn.Module):
     def __init__(self, vocab_size, embedding_dim, hidden_dim, dropout=0):
         super().__init__()
+        # gloabl embedding
         self.embedding = torch.nn.Embedding(vocab_size, embedding_dim, padding_idx=0)   # replace with word to vec
-        self.forward_lstm = torch.nn.LSTM(embedding_dim, hidden_dim, 2, batch_first=True, dropout=dropout)
+        
+        # contexual embedding
+        self.forward_lstm_1 = torch.nn.LSTM(embedding_dim, hidden_dim, 1, batch_first=True, dropout=dropout)
+        self.forward_lstm_2 = torch.nn.LSTM(hidden_dim, hidden_dim, 1, batch_first=True, dropout=dropout)
+        self.bacward_lstm_1 = torch.nn.LSTM(embedding_dim, hidden_dim, 1, batch_first=True, dropout=dropout)
+        self.bacward_lstm_2 = torch.nn.LSTM(hidden_dim, hidden_dim, 1, batch_first=True, dropout=dropout)
+        
+        # task specific
+        self.weights = torch.nn.Parameter(torch.ones(2, dtype=torch.float))
+        self.scale = torch.nn.Parameter(torch.zeros(1))
 
     def forward(self, x):
         # global embeddings
         x = self.embedding(x)
 
         # forward contexual embedding
-        h, _ = self.forward_lstm(x)
-        return h
-    
+        f1, _ = self.forward_lstm_1(x)
+        f2, _ = self.forward_lstm_2(f1)
+
+        # backward contexual embedding
+        b1, _ = self.bacward_lstm_1(x.flip(1))
+        b2, _ = self.bacward_lstm_2(b1)
+
+        # concat
+        e1 = torch.cat([f1, b1.flip(1)], dim=2)
+        e2 = torch.cat([f2, b2.flip(1)], dim=2)
+
+        # combination
+        w = self.weights.softmax(dim=0)
+        embeddings = self.scale * (w[0] * e1 + w[1] * e2)
+        return embeddings
+
+
+
+class ELMoSentiment(torch.nn.Module):
+    def __init__(self, vocab_size, embedding_dim, hidden_dim, sen_embd_dim, dropout=0):
+        super().__init__()
+        self.elmo = ELMo(vocab_size, embedding_dim, hidden_dim, dropout)
+        self.linear_classifier = torch.nn.Linear(sen_embd_dim, 2)
+
+    def forward(self, x):
+        # word embeddings
+        x = self.elmo(x)
+        
+        # sentence_embeddings
+        stentence_embedding = x.mean(dim=1)
+
+        # classification
+        logits = self.linear_classifier(stentence_embedding)
+        return logits
+
 
 
 if __name__ == "__main__":
@@ -26,9 +68,9 @@ if __name__ == "__main__":
     dataset = datasets.load_dataset('sst')
     vocab = create_vocab_stt(dataset["train"])
     train_dataset = SSTDataset(dataset["train"], vocab)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=32, shuffle=True, collate_fn=collate_fn_stt)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, collate_fn=collate_fn_stt)
 
-    model = ELMo(len(vocab), 300, 400).to(device)
+    model = ELMoSentiment(len(vocab), 300, 400, 800).to(device)
     for X, y in tqdm(train_loader):
         X = X.to(device)
         y = y.to(device)
